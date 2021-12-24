f8703d021a4fdbf1f06868d3fd470868508bd528
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-12-23 23:51:19 -0300

    [add] advances

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
-function App() {
+/* const HelloWorld = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
+      <h3>{props.title}</h3>
+      {props.mytext}
+    </div>
+  )
+} */
+
+class HelloWorld extends react.Component {
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.title}</h3>
+          <p>{this.props.mytext}</p>
+          <button onClick={this.toggleShow}>Toggle show</button>
+        </div>
+      )
+    }
+    else return (
+      <h1>
+        The are not elements
+        <button onClick={this.toggleShow}>ToggleShow</button>
+      </h1>
+    )
+  }
+}
+
+const App = () => {
+  return (
+    <div>
+      this is my component:
+      <HelloWorld mytext="Hello santi" title="component 1" />
+      <HelloWorld mytext="Hola mundo" title="component 2" />
+      <HelloWorld mytext="Hello!!" title="component 3" />
     </div>
   );
 }
